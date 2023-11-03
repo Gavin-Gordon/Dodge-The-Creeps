dea49a02d2dcfe01589d9f3dc11617a0af6e4354
Gavin Gordon <109978304+Gavin-Gordon@users.noreply.github.com>
2023-11-03 14:40:06 -0400

    Added files for "Dodge The Creeps"
    Added, player, mobs, timers and licenses.

---
+++ Main.gd
@@ -0,0 +1,34 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	pass # Replace with function body.
+
+func new_game():
+	score = 0
+$Player.start($StartPosition.position)
+$StartTimer.start()
+
+func _on_start_timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_score_timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_mob_timer_timeout():
+	pass # Replace with function body.
